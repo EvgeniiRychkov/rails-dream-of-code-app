bdccbd3eccd0242c5ccb7bd0efbc5684a74e7d83
Evgenii Rychkov <evgeniirychkov@Evgeniis-MacBook-Pro.local>
2025-04-30 21:08:49 -0700

    Controllers and routing with non-resourceful routes multiple model requests query params and JSON requests

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :courses
   resources :coding_classes
   resources :trimesters, only: %i[index show]
+  get '/dashboard', to: 'admin_dashboard#index'
   get 'up' => 'rails/health#show', as: :rails_health_check
 
   root 'home#index'
